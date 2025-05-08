a8bcfefe7de2b04052edb39966b45a368d22f548
= <=>
2025-05-08 18:53:08 +0800

    Second Commit

--- resources/views/dashboards/admin_book_manage.blade.php
+++ resources/views/dashboards/admin_book_manage.blade.php
@@ -51,14 +51,14 @@
 
         <!-- Navigation -->
         <nav class="flex-1 overflow-y-auto p-4 space-y-1">
-            <a href="{{ route('admin.dashboard') }}" class="flex items-center space-x-3 p-3 rounded-lg hover:bg-gray-100 active-nav">
+            <a href="{{ route('admin.dashboard') }}" class="flex items-center space-x-3 p-3 rounded-lg hover:bg-gray-100">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                 </svg>
                 <span>Dashboard</span>
             </a>
 
-            <a href="{{--{{ route('books.index') }}--}}" class="flex items-center space-x-3 p-3 rounded-lg hover:bg-gray-100">
+            <a href="{{--{{ route('books.index') }}--}}" class="flex items-center space-x-3 p-3 rounded-lg hover:bg-gray-100 active-nav">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                 </svg>
